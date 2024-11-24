bd5be485a8d753841f0623c5d96f9714ffeb705d
Rishab Pangal <90218399+rpangal@users.noreply.github.com>
2024-11-24 13:18:35 -0800

    Making lambda function more modular

---
+++ src/api/types.ts
@@ -0,0 +1,29 @@
+export type Track = 'Performance track' | 'Access control track' | 'High assurance track' | 'ML inside track';
+
+export interface TrackSelection {
+    plannedTracks: Track[];
+}
+
+export interface PackageData {
+  Name: string;
+  Content?: string;
+  URL?: string;
+  debloat?: boolean;
+  JSProgram?: string; 
+}
+
+export interface PackageMetadata {
+  Name: string;
+  Version: string;
+  ID: string;
+}
+
+export interface Package {
+  metadata: PackageMetadata;
+  data: PackageData;
+}
+
+export interface PackageCost {
+  standaloneCost: number;
+  totalCost: number;
+}
