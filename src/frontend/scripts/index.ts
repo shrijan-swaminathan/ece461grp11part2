6450f795a627a7f1477b130aeccae2aea3d41766
Rishab Pangal <90218399+rpangal@users.noreply.github.com>
2024-11-23 19:39:38 -0800

    testing POST /package for web interface

--- src/frontend/scripts/index.ts
+++ src/frontend/scripts/index.ts
@@ -3,22 +3,58 @@ async function uploadModule(): Promise<void> {
     const formData: FormData = new FormData(form);
 
     try {
-        const response: Response = await fetch('/api/upload', {
+        // get module name from form
+        const moduleNameElement: HTMLInputElement | null = document.getElementById('moduleName') as HTMLInputElement;
+        // get debloat checkbox from form
+        const debloatElement: HTMLInputElement | null = document.getElementById('debloat') as HTMLInputElement;
+        // get zip file from form
+        const fileInput: HTMLInputElement | null = document.getElementById('moduleFile') as HTMLInputElement;
+        if (!fileInput || !fileInput.files || fileInput.files.length === 0) {
+            throw new Error('No file selected');
+        }
+        // convert to base64 string
+        const file = fileInput.files[0];
+        const base64String = await readFileAsBase64(file);
+        formData.append('moduleFile', base64String);
+        const requestBody = {
+            Content: base64String,
+            debloat: debloatElement.checked,
+            Name: moduleNameElement.value,
+        };
+        console.log(base64String);
+        const response: Response = await fetch('https://dofogoenof.execute-api.us-east-2.amazonaws.com/MainStage/package', {
             method: 'POST',
-            body: formData
+            body: JSON.stringify(requestBody),
         });
         const result: { message: string } = await response.json();
         const uploadResultElement: HTMLElement | null = document.getElementById('uploadResult');
         if (uploadResultElement) {
             uploadResultElement.innerHTML = `Upload status: ${result.message}`;
         }
-    } catch (error) {
+    } catch (error: any) {
         const uploadResultElement: HTMLElement | null = document.getElementById('uploadResult');
         if (uploadResultElement) {
-            uploadResultElement.innerHTML = 'Upload failed. Please try again.';
+            uploadResultElement.innerHTML = error!.message || 'Upload failed. Please try again.';
         }
         console.error('Upload error:', error);
     }
+}
+
+
+
+function readFileAsBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if (typeof reader.result === 'string') {
+                resolve(reader.result);
+            } else {
+                reject(new Error('Failed to read file as base64 string'));
+            }
+        };
+        reader.onerror = () => reject(new Error('Error reading file'));
+        reader.readAsDataURL(file);
+    });
 }
 
 async function searchModules(): Promise<void> {
